1de5a631e284f60a74e64c9fd02fbd0b610571c7
Bashar Mestrih <mestbashar@gmail.com>
2025-07-06 22:31:05 +0300

    Add files via upload

---
+++ lib/app/data/models/RandomList.dart
@@ -0,0 +1,48 @@
+import 'package:my_house_app/app/data/models/Home_model.dart';
+
+final List<HomeModel> exampleHomes = [
+  HomeModel(
+    id: 1,
+    name: 'Modern Apartment', 
+    location: 'Damascus',
+    imgUrl: 'https://tse2.mm.bing.net/th/id/OIP.b7ibTjl2QGU1MNTFyeV-FAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3',
+    description: 'Spacious and sunny apartment in central Damascus.',
+    Cost: 500,
+  ),
+  HomeModel(
+    id: 2,
+    name: 'Cozy Cottage',
+    location: 'Homs',
+    imgUrl: 'https://tse2.mm.bing.net/th/id/OIP.b7ibTjl2QGU1MNTFyeV-FAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3',
+    description: 'Quiet 2-bedroom cottage near the park.',
+    Cost: 500,
+
+  ),
+  HomeModel(
+    id: 3,
+    name: 'Luxury Villa',
+    location: 'Lattakia',
+    imgUrl: 'https://tse2.mm.bing.net/th/id/OIP.b7ibTjl2QGU1MNTFyeV-FAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3',
+    description: 'Private villa with pool and sea view.',
+    Cost: 500,
+
+  ),
+  HomeModel(
+    id: 4,
+    name: 'Budget Studio',
+    location: 'Aleppo',
+    imgUrl: 'https://tse2.mm.bing.net/th/id/OIP.b7ibTjl2QGU1MNTFyeV-FAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3',
+    description: 'Affordable studio ideal for students.',
+    Cost: 500,
+
+  ),
+  HomeModel(
+    id: 5,
+    name: 'Family House',
+    location: 'Tartous',
+    imgUrl: 'https://tse2.mm.bing.net/th/id/OIP.b7ibTjl2QGU1MNTFyeV-FAHaE8?rs=1&pid=ImgDetMain&o=7&rm=3',
+    description: 'Spacious family home with garden.',
+    Cost: 500,
+
+  ),
+];
