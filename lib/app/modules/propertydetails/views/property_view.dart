3304efd97f843a96f913a2a54b77691ed492b604
Bashar Mestrih <mestbashar@gmail.com>
2025-07-10 17:11:36 +0300

    Update property_view.dart

--- lib/app/modules/propertydetails/views/property_view.dart
+++ lib/app/modules/propertydetails/views/property_view.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-import 'package:my_house_app/app/core/theme/colors2.dart';
 import 'package:my_house_app/app/modules/propertydetails/views/widgets/Appbar.dart';
 import 'package:my_house_app/app/modules/propertydetails/views/widgets/PropertyFeatures.dart';
 import 'package:my_house_app/app/modules/propertydetails/views/widgets/PropertyGallery.dart';
