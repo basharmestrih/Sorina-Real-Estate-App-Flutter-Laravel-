f9cf42f2013c251fc2e7a04d39fab47a57601ddd
Bashar Mestrih <mestbashar@gmail.com>
2025-07-10 20:07:29 +0300

    Update Appbar.dart

--- lib/app/modules/homemanagement/view/widgets/Appbar.dart
+++ lib/app/modules/homemanagement/view/widgets/Appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
@@ -10,7 +11,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       automaticallyImplyLeading: false,
       title:  Text(
-        'Property Manager',
+        'manager_page_title'.tr,
        style: Theme.of(context).textTheme.titleLarge,
       ),
       centerTitle: true,
