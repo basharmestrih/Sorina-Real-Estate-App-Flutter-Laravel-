d16c316d2222fdbeb4bb17958481b0432583ddbb
Bashar Mestrih <mestbashar@gmail.com>
2025-07-10 17:07:21 +0300

    Update colors.dart

--- lib/app/core/theme/colors.dart
+++ lib/app/core/theme/colors.dart
@@ -2,14 +2,29 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // static const Color primary = Color(0xFF496C6D);
-  static const Color primary = Color(0xFF4A6A7F);
-  static const Color secondary = Color(0x26496C6D); // 15% opacity
+  static const Color primary = Colors.white;
+  // target amber color
+  static const Color secondary = Color(0xFFFFDE59);
+  
+  static const fontcolor = Colors.black;
+  static const secondaryfontcolor =Colors.grey;
+  static const numbersfontcolor =Color(0xFF1E88E5);
+
+
   // static const Color surface = Color.fromRGBO(252, 253, 253, 1);
   static const Color surface = Color.fromRGBO(233, 238, 238, 0.9);
-  static const Color responsiveButtonColor = Color.fromRGBO(233, 238, 238, 1);
+  static const Color responsiveButtonColor = Color(0xFFFFDE59);
 
   static const Color white = Color.fromRGBO(252, 253, 253, 1);
-  static const Color background= Color.fromRGBO(5, 57, 98, 0.8);
+  static const Color black = Colors.black;
+  static const Color red = Colors.red;
+  static const Color grey = Colors.grey;
+  static  Color lightGrey = Color(0xFFF5F5F5);
+    static  Color boldGrey = Color(0xEEEEEE);
+
+
+
+  static const Color background= Color.fromRGBO(16, 25, 32, 0.8);
   static const Color background1= Color.fromRGBO(135, 165, 200, 1.0);
   static const Color orange = Color.fromRGBO(255, 238, 126, 28);
 }
